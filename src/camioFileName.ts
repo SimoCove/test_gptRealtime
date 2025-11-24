b1d609b745cd97ea6ed1f448ab522527bfef9f9e
Simone Covelli <simone.covelli03@gmail.com>
2025-11-24 19:07:19 +0100

    add new camio file

--- src/camioFileName.ts
+++ src/camioFileName.ts
@@ -1,7 +1,9 @@
-export const camioFileName: string = "Islet";
+export const camioFileName: string = "Islet2";
 // Aeroplano
 // Airplane
 // Automobile
 // Car
 // House_with_rainbow
+// House_with_rainbow2
 // Islet
+// Islet2
